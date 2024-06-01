dc19cd6e94d705512d430b5e6dd347a48e6ae39e
mmustra <marin.mustra@gmail.com>
2024-06-01 14:39:16 +0200

    ci: add codecov

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,14 @@ import { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageReporters: ['text', 'cobertura', 'html'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
   transform: {
     /* eslint-disable @typescript-eslint/naming-convention */
     '\\.[jt]sx?$': [
