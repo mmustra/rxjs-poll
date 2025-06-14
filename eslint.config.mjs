91f31a6dc59387a2baffbb37d396f195adb3ef04
mmustra <marin.mustra@gmail.com>
2025-06-14 11:38:26 +0200

    chore: move to new configuration format to support ESlint v9+

---
+++ eslint.config.mjs
@@ -0,0 +1,81 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import importPlugin from 'eslint-plugin-import';
+import jestFormatting from 'eslint-plugin-jest-formatting';
+import xo from 'eslint-config-xo';
+import xoTypescript from 'eslint-config-xo-typescript/space';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsparser,
+      parserOptions: {
+        tsconfigRootDir: './ts-configs/eslint',
+        project: 'tsconfig.json',
+      },
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettier,
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort,
+      import: importPlugin,
+      'jest-formatting': jestFormatting,
+    },
+    rules: {
+      ...xo.rules,
+      ...xoTypescript.rules,
+      ...jestFormatting.configs.strict.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-warning-comments': 'off',
+      'capitalized-comments': 'off',
+      'prefer-object-spread': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'max-nested-callbacks': 'off',
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxBOF: 0,
+          maxEOF: 0,
+        },
+      ],
+    },
+  },
+];
